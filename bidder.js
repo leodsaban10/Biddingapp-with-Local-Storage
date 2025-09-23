9b982246d4e6381441bea201b17ac1f1e5c744e8
Leo Saban <leodsaban@gmail.com>
2025-09-23 09:37:59 -0600

    declared the variables to reference the html elements

---
+++ bidder.js
@@ -0,0 +1,18 @@
+let bidder1Total = 0;
+let bidder2Total = 0;
+
+// display elements
+const bidder1TotalDisplay = document.getElementById('bidder1-total');
+const bidder2TotalDisplay = document.getElementById('bidder2-total');
+const highestBidderDisplay = document.getElementById('highest-bidder');
+const highestBidDisplay = document.getElementById('highest-bid');
+
+// input elements
+const bidder1Input = document.getElementById('bidder1-input');
+const bidder2Input = document.getElementById('bidder2-input');
+
+// button elements
+const bidder1Btn = document.getElementById('bidder1-btn');
+const bidder2Btn = document.getElementById('bidder2-btn');
+const clearBtn = document.getElementById('clear-bids-btn');
+
